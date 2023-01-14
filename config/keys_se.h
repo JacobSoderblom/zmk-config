72875d9c66ec0f1008dc7352866377bca894853b
Jacob Söderblom <jacob.soderblom@protonmail.com>
2023-01-14 15:51:45 +0100

    fix

--- config/keys_se.h
+++ config/keys_se.h
@@ -138,14 +138,14 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_AT   RA(SE_2)    // @
-#define SE_PND  RA(SE_3)    // £
-#define SE_DLR  RA(SE_4)    // $
-#define SE_EURO RA(SE_5)    // €
-#define SE_LCBR RA(SE_7)    // {
-#define SE_LBRC RA(SE_8)    // [
-#define SE_RBRC RA(SE_9)    // ]
-#define SE_RCBR RA(SE_0)    // }
+#define SE_AT   RA(SE_N2)    // @
+#define SE_PND  RA(SE_N3)    // £
+#define SE_DLR  RA(SE_N4)    // $
+#define SE_EURO RA(SE_N5)    // €
+#define SE_LCBR RA(SE_N7)    // {
+#define SE_LBRC RA(SE_N8)    // [
+#define SE_RBRC RA(SE_N9)    // ]
+#define SE_RCBR RA(SE_N0)    // }
 #define SE_BSLS RA(SE_PLUS) // (backslash)
 // Row 2
 #define SE_TILD RA(SE_DIAE) // ~ (dead)
